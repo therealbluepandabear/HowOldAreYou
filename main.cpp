44975f85c7f433c2e699de31d6be59490d5f7605
xTheCoderx <justaguyoney@gmail.com>
2021-10-14 12:24:38 +1300

    User can now in put year of birth, month of birth, and day of birth which are all saved to an Integer.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 
+#include <iostream>
+
 int main() {
-    
+    int year, month, day;
+
+    std::cout << "Enter year of birth: " << std::endl;
+
+    std::cin >> year;
+
+    std::cout << "Enter month of birth: " << std::endl;
+
+    std::cin >> month;
+
+    std::cout << "Enter day of birth: " << std::endl;
+
+    std::cin >> day;
+
     return 0;
 }
